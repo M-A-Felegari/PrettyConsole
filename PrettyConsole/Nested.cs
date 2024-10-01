e3a55332200bacf841d19a88633eafe2b296a4ba
M-A-Felegari <M-A-Felegari@hotmail.com>
2024-10-01 13:08:29 +0330

    refactor:change dotnet to dotnet 8

--- PrettyConsole/Nested.cs
+++ PrettyConsole/Nested.cs
@@ -1,32 +1,31 @@
-﻿namespace PrettyConsole
+﻿namespace PrettyConsole;
+
+public class Nested
 {
-    public class Nested
+    public static string SimpleNested(short order, string text)
     {
-        public static string SimpleNested(short order, string text)
+        string output = String.Empty;
+        int distance = (order - 1) * 5;
+        for (int i = 0; i < distance; i++)
         {
-            string output = String.Empty;
-            int distance = (order - 1) * 5;
-            for (int i = 0; i < distance; i++)
-            {
-                output += " ";
-            }
-            output += $"■ {text}";
-            return output;
+            output += " ";
         }
-        public static string DashedNested1(short order, string text)
+        output += $"■ {text}";
+        return output;
+    }
+    public static string DashedNested1(short order, string text)
+    {
+        string output = String.Empty;
+        int distance = (order - 2) * 5;
+        for (int i = 0; i < distance; i++)
         {
-            string output = String.Empty;
-            int distance = (order - 2) * 5;
-            for (int i = 0; i < distance; i++)
-            {
-                output += " ";
-            }
-            if (order > 1)
-            {
-                output += "█====";
-            }
-            output += $"■ {text}";
-            return output;
+            output += " ";
         }
+        if (order > 1)
+        {
+            output += "█====";
+        }
+        output += $"■ {text}";
+        return output;
     }
 }
